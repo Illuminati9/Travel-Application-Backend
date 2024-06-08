108335684bab64aea0ee291c217fe20e2c69e858
Sridhar Suthapalli <sutapalli.sns@gmail.com>
2024-06-08 18:07:55 +0530

    Random commit

--- index.js
+++ index.js
@@ -24,14 +24,7 @@ require("dotenv").config({ path: ".env" });
 
 const port = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("dev"));
 app.use(express.json());
